9cd9a8f44261e6385af86c4f65e20609fc729f0e
DESKTOP-45SMRRK\JUNGLE <rurain7763@gmail.com>
2026-09-10 19:43:14 +0900

    chore: 프로젝트 폴더 구조 정리 및 Premake 설정 분리
    - Week2 소스를 WEEK3TEAM7 디렉터리로 이동
    - ShaderW0.hlsl을 저장소 루트로 이동
    - 프로젝트 Premake 설정을 분리하고 루트에서 include
    - Json 헤더 검색 경로 추가
    - Release 빌드 최적화를 Speed에서 Off로 변경

---
+++ WEEK3TEAM7/premake5.lua
@@ -0,0 +1,54 @@
+project "WEEK3TEAM7"
+    kind "WindowedApp"
+    language "C++"
+    cppdialect "C++20"
+    systemversion "latest"
+    characterset "Unicode"
+
+    targetdir ("bin/%{cfg.platform}/%{cfg.buildcfg}")
+    objdir ("bin-int/%{cfg.platform}/%{cfg.buildcfg}")
+    debugdir "%{wks.location}"
+
+    files {
+        "**.cpp",
+        "**.h",
+    }
+
+    includedirs {
+        ".",
+        "ImGui",
+        "Json"
+    }
+
+    links {
+        "d3d11",
+        "d3dcompiler",
+        "dwrite",
+        "d2d1",
+        "dxgi",
+        "dwmapi",
+        "gdi32",
+        "imm32",
+        "user32"
+    }
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "On"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "Off"
+        symbols "On"
+
+    filter "system:windows"
+        defines {
+            "UNICODE",
+            "_UNICODE",
+            "WIN32_LEAN_AND_MEAN",
+        }
+
+filter "toolset:msc*"
+    buildoptions { "/utf-8" }
+
+filter {}
